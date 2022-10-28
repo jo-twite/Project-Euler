213c6b060c5f0869edc72bc17d205ca9bdf7a54c
jo-twite <jo-twite@hotmail.com>
2022-10-28 17:06:40 +0200

    problem 1 solved

---
+++ com/src/problem0001/Main0001.java
@@ -0,0 +1,24 @@
+package problem0001;
+
+
+// https://projecteuler.net/problem=1
+
+public class Main0001 {
+
+
+    public static void main(String[] args) {
+        System.out.println(multiplesOfThreeAndFive(1000));
+    }
+
+    private static int multiplesOfThreeAndFive(int n) {
+        int multiplesOfThree = n/3;
+        int multiplesOfFive = n/5;
+        int multiplesOfFifteen = n/15;
+
+        int sumOfmultiplesOfThree = multiplesOfThree*(multiplesOfThree + 1)/2 * 3;
+        int sumOfmultiplesOfFive = multiplesOfFive*(multiplesOfFive + 1)/2 * 5;
+        int sumOfmultiplesOfFifteen = multiplesOfFifteen*(multiplesOfFifteen + 1)/2 * 15;
+
+        return sumOfmultiplesOfThree + sumOfmultiplesOfFive - ((n%5 == 0 || n%3 == 0)? n : 0) - sumOfmultiplesOfFifteen;
+    }
+}
